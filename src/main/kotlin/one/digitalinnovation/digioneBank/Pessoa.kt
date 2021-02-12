540c548f980258993f91c93a75c1b535d42d586b
Sandro37 <alessandro37@outlook.com.br>
2021-02-12 15:15:34 -0300

    atualizando o modificador de acesso da propriedade cpf e deixando privada o set - Class Pessoa

--- src/main/kotlin/one/digitalinnovation/digioneBank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digioneBank/Pessoa.kt
@@ -2,13 +2,16 @@ package one.digitalinnovation.digioneBank
 
 class Pessoa {
     var nome:String = "Jether"
+
     var cpf:String = "123.123.123-11"
+    private  set
 
 }
 
 fun main() {
     val jether = Pessoa()
-
+    jether.nome = "ama"
+    jether.cpf = "4"
     println(jether.nome)
     println(jether.cpf)
 }
