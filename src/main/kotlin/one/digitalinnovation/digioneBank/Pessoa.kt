58b4dfec79847c455147f5290d5e9c3ee9611a43
Sandro37 <alessandro37@outlook.com.br>
2021-02-14 19:50:25 -0300

    atualizando a classe pessoa e funcionario
    - tornando a classe Pessoa abstract
    - tornado a classe Funcionario abstract
    - criando um método abstract calculoAuxilio em Funcionario

--- src/main/kotlin/one/digitalinnovation/digioneBank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digioneBank/Pessoa.kt
@@ -1,19 +1,9 @@
 package one.digitalinnovation.digioneBank
 
-class Pessoa {
-    var nome:String = "Jether"
-    var cpf:String = "123.123.123-11"
-    private  set
+abstract class Pessoa(
+    val nome:String,
+    val cpf:String
+){
 
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
 }
 
-fun main() {
-    val jether = Pessoa()
-    println(jether.nome)
-    println(jether.cpf)
-
-    println(jether.pessoaInfo())
-}
