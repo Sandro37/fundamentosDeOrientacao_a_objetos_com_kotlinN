8c55f30efbf776f348abd81e55232b183f7db5e3
Sandro37 <alessandro37@outlook.com.br>
2021-02-14 22:21:47 -0300

    Update TesteFuncionarioGerente.kt

--- src/main/kotlin/one/digitalinnovation/digioneBank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digioneBank/testes/TesteFuncionarioGerente.kt
@@ -3,6 +3,8 @@ package one.digitalinnovation.digioneBank.testes
 import one.digitalinnovation.digioneBank.Gerente
 
 fun main() {
-    val maria = Gerente("Maria Rodrigues", "1234567899", 5000.0)
+    val maria = Gerente("Maria Rodrigues", "1234567899", 5000.0,"senha123")
+
     ImprimeRelatorioFuncionario.imprime(maria)
+    TesteAutenticacao().autenticar(maria)
 }
