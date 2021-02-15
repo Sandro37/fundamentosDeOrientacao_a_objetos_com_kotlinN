9764303bb38ed9281960dd0c1afb5034bbeda01d
Sandro37 <alessandro37@outlook.com.br>
2021-02-14 22:21:16 -0300

    Create TesteAutenticacao.kt
    - Criando função autenticar com parametro da interface Logavel e imprime  TRUE ou FALSE

---
+++ src/main/kotlin/one/digitalinnovation/digioneBank/testes/TesteAutenticacao.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digioneBank.testes
+
+import one.digitalinnovation.digioneBank.Logavel
+
+class TesteAutenticacao {
+    fun autenticar(logavel: Logavel) = println(logavel.login())
+}
